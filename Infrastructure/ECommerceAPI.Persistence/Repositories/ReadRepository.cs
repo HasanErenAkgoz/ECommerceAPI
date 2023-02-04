6731c610beaeb719a36b373b6debe08c9445245b
Hasan Eren Akgöz <eren34akgoz@gmail.com>
2023-02-04 16:39:10 +0300

    Ef Core Tracking Performance Optimization

--- Infrastructure/ECommerceAPI.Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/ReadRepository.cs
@@ -2,6 +2,7 @@
 using ECommerceAPI.Domain.Entities.Common;
 using ECommerceAPI.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System.Linq.Expressions;
 
 namespace ECommerceAPI.Persistence.Repositories
@@ -17,16 +18,38 @@ namespace ECommerceAPI.Persistence.Repositories
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
-            => Table;
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
 
-        public async Task<T> GetByIdAsync(string id)
-            => await Table.FindAsync(Guid.Parse(id));
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return await query.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        }
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> expression)
-            => await Table.FirstOrDefaultAsync(expression);
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> expression, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return await query.FirstOrDefaultAsync(expression);
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression)
-            => Table.Where(expression);
+            
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression, bool tracking = true)
+        {
+            var query = Table.Where(expression);
+            if(!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
     }
 }
