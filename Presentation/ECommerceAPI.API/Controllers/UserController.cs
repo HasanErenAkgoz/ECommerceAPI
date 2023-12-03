be0116077819f832af7293e1e9c49ca037784fba
Hasan Eren Akgöz <eren34akgoz@gmail.com>
2023-12-03 23:49:35 +0300

    Google Login Added to the Project

--- Presentation/ECommerceAPI.API/Controllers/UserController.cs
+++ Presentation/ECommerceAPI.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceAPI.Application.Features.User.Commands.Create;
+using ECommerceAPI.Application.Features.User.Commands.GoogleLogin;
 using ECommerceAPI.Application.Features.User.Commands.Login;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +28,13 @@ namespace ECommerceAPI.API.Controllers
         public async Task<IActionResult> Login(LoginCommandRequest loginCommandRequest)
         {
             LoginCommandResponse response = await _mediator.Send(loginCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpPost("google-login")]
+        public async Task<IActionResult> GoogleLogin(GoogleLoginCommandRequest googleLoginCommandRequest)
+        {
+            GoogleLoginCommandResponse response = await _mediator.Send(googleLoginCommandRequest);
             return Ok(response);
         }
      
